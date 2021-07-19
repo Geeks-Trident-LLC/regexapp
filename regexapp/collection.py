4ce5f8dcaf1b19203b3b8cf95472a3fb4c86d5a3
Tuyen Mathew Duong <tuyen@geekstrident.com>
2021-07-19 14:40:17 -0700

    implement var_name and base_pattern attributes for regexapp.collection.ElementPattern class

--- regexapp/collection.py
+++ regexapp/collection.py
@@ -182,6 +182,11 @@ class ElementPatternError(Exception):
 class ElementPattern(str):
     """Use to convert element data to regex pattern
 
+    Attributes
+    ----------
+    var_name (str): a regex variable name
+    base_pattern (str): a base regex pattern before enclosing pattern var name.
+
     Parameters
     ----------
     text (str): a text.
@@ -207,6 +212,8 @@ class ElementPattern(str):
 
     """
     def __new__(cls, text):
+        cls.var_name = ''
+        cls.base_pattern = ''
         data = str(text)
         if data:
             pattern = cls.get_pattern(data)
@@ -507,6 +514,8 @@ class ElementPattern(str):
         str: new pattern with variable name.
         """
         if name:
+            cls.var_name = name
+            cls.base_pattern = pattern
             new_pattern = '(?P<{}>{})'.format(name, pattern)
             return new_pattern
         return pattern
