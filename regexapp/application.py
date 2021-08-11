5cbbac739276d6238a7b23e962c9e0ce63696ce8
Tuyen Mathew Duong <tuyen@geekstrident.com>
2021-08-11 09:26:57 -0700

    improve regexapp.application.create_messagebox function

--- regexapp/application.py
+++ regexapp/application.py
@@ -40,18 +40,56 @@ def get_relative_center_location(parent, width, height):
     return x, y
 
 
-def create_msgbox(error=None, info=None, title=None):
+def create_msgbox(title=None, error=None, warning=None, info=None,
+                  question=None, okcancel=None, retrycancel=None,
+                  yesno=None, yesnocancel=None, **options):
     """create tkinter.messagebox
     Parameters
     ----------
-    error (str): an error message.  Default is None.
-    info (str): an information message.  Default is None.
     title (str): a title of messagebox.  Default is None.
+    error (str): an error message.  Default is None.
+    warning (str): a warning message. Default is None.
+    info (str): an information message.  Default is None.
+    question (str): a question message.  Default is None.
+    okcancel (str): an ok or cancel message.  Default is None.
+    retrycancel (str): a retry or cancel message.  Default is None.
+    yesno (str): a yes or no message.  Default is None.
+    yesnocancel (str): a yes, no, or cancel message.  Default is None.
+    options (dict): options for messagebox.
+
+    Returns
+    -------
+    any: a string or boolean result
     """
     if error:
-        messagebox.showerror(title=title, message=error)
+        # a return result is a "ok" string
+        result = messagebox.showerror(title=title, message=error, **options)
+    elif warning:
+        # a return result is a "ok" string
+        result = messagebox.showwarning(title=title, message=warning, **options)
+    elif info:
+        # a return result is a "ok" string
+        result = messagebox.showinfo(title=title, message=info, **options)
+    elif question:
+        # a return result is a "yes" or "no" string
+        result = messagebox.askquestion(title=title, message=question, **options)
+    elif okcancel:
+        # a return result is boolean
+        result = messagebox.askokcancel(title=title, message=okcancel, **options)
+    elif retrycancel:
+        # a return result is boolean
+        result = messagebox.askretrycancel(title=title, message=retrycancel, **options)
+    elif yesno:
+        # a return result is boolean
+        result = messagebox.askyesno(title=title, message=yesno, **options)
+    elif yesnocancel:
+        # a return result is boolean or None
+        result = messagebox.askyesnocancel(title=title, message=yesnocancel, **options)
     else:
-        messagebox.showinfo(title=title, message=info)
+        # a return result is a "ok" string
+        result = messagebox.showinfo(title=title, message=info, **options)
+
+    return result
 
 
 class Data:
