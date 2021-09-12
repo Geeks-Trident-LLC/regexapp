579720e5097577e994e0459133fd4d76c92c314d
Tuyen Mathew Duong <tuyen@geekstrident.com>
2021-09-11 17:12:41 -0700

    version: update regexapp to version 0.3.0

--- regexapp/config.py
+++ regexapp/config.py
@@ -8,7 +8,7 @@ from pathlib import PurePath
 
 import yaml
 
-__version__ = '0.2.2'
+__version__ = '0.3.0'
 version = __version__
 __edition__ = 'Community'
 edition = __edition__
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='regexapp',
-    version='0.2.3',
+    version='0.3.0',
     license='BSD-3-Clause',
     license_files=['LICENSE'],
     description='The application to generate regex pattern.',
