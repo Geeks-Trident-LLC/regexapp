6e0b52d473fe12d21b9bd4469735d1767fe3c54f
Tuyen Mathew Duong <tuyen@geekstrident.com>
2021-09-20 16:34:28 -0700

    unittest: add ElementPattern.remove_tail_of_string unit test

--- tests/unit/test_collection.py
+++ tests/unit/test_collection.py
@@ -256,13 +256,38 @@ class TestElementPattern:
             ),
         ]
     )
-    def test_remove_start_of_pattern(self, data, expected_pattern,
+    def test_remove_head_of_pattern(self, data, expected_pattern,
                                      expected_pattern_after_removed):
         pattern = ElementPattern(data)
         assert pattern == expected_pattern
 
         removed_head_of_str_pattern = pattern.remove_head_of_string()
         assert removed_head_of_str_pattern == expected_pattern_after_removed
+
+    @pytest.mark.parametrize(
+        (
+            'data', 'expected_pattern', 'expected_pattern_after_removed'
+        ),
+        [
+            (
+                'words(tail)',
+                '[a-zA-Z0-9]+( [a-zA-Z0-9]+)*$',
+                '[a-zA-Z0-9]+( [a-zA-Z0-9]+)*'
+            ),
+            (
+                'words(var_v1, tail_whitespace)',
+                '(?P<v1>[a-zA-Z0-9]+( [a-zA-Z0-9]+)*)\\s*$',
+                '(?P<v1>[a-zA-Z0-9]+( [a-zA-Z0-9]+)*)'
+            ),
+        ]
+    )
+    def test_remove_tail_of_pattern(self, data, expected_pattern,
+                                    expected_pattern_after_removed):
+        pattern = ElementPattern(data)
+        assert pattern == expected_pattern
+
+        removed_tail_of_str_pattern = pattern.remove_tail_of_string()
+        assert removed_tail_of_str_pattern == expected_pattern_after_removed
 
 
 class TestLinePattern:
