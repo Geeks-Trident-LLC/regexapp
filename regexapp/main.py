6b965a6e4801406696026ec327c2a3e9949e5e9f
Tuyen Mathew Duong <tuyen@geekstrident.com>
2022-04-29 14:55:16 -0700

    baseline: refactor main.show_dependency

--- regexapp/main.py
+++ regexapp/main.py
@@ -10,6 +10,8 @@ from regexapp.application import Application
 from regexapp import RegexBuilder
 from regexapp.core import enclose_string
 
+from regexapp.utils import Printer
+
 
 def run_gui_application(options):
     """Run regexapp GUI application.
@@ -46,9 +48,7 @@ def show_dependency(options):
             lst.append('  + Package: {0[package]}'.format(pkg))
             lst.append('             {0[url]}'.format(pkg))
 
-        width = max(len(item) for item in lst)
-        txt = '\n'.join('| {1:{0}} |'.format(width, item) for item in lst)
-        print('+-{0}-+\n{1}\n+-{0}-+'.format(width * '-', txt))
+        Printer.print(lst)
         sys.exit(0)
 
 
